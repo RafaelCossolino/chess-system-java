16209d04c124de8e37f660ace952211a8688f49e
RafaelCossolino <rafaelcossolino@gmail.com>
2020-06-09 19:08:52 -0300

    Tabuleiro

--- src/chesssystem/ChessSystem.java
+++ src/chesssystem/ChessSystem.java
@@ -2,12 +2,15 @@
 package chesssystem;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class ChessSystem {
 
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+        
+        
     }
     
 }
